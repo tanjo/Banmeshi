e4c9f8222341b58d37dbc1ad77c606e656d18450
tanjo <tanjo@thebitcellar.com>
2014-10-01 10:10:57 +0900

    184万を誇示しているので修正 http://gyazo.com/e52f1cbf92368cf56e938c2747ed6b52.png

--- Banmeshi/background.js
+++ Banmeshi/background.js
@@ -1,4 +1,4 @@
 chrome.browserAction.onClicked.addListener(function(activeTab) {
-  url='http://cookpad.com/recipe/' + Math.floor( Math.random() * 1800000 );
+  url='http://cookpad.com/recipe/' + Math.floor( Math.random() * 1840000 );
   chrome.tabs.create( { url: url} );
 });
