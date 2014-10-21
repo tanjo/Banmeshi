cce8a9d9841082b7a21d12bff13cb1dae4bbb22b
tanjo <tanjo@thebitcellar.com>
2014-10-21 21:14:43 +0900

    タイトルマッチでレシピが見つからなかったか確認する

--- Banmeshi/background.js
+++ Banmeshi/background.js
@@ -1,4 +1,38 @@
+function create() {
+  var url='http://cookpad.com/recipe/' + Math.floor( Math.random() * 1840000 );
+  return url;
+}
+
+function run() {
+  count++;
+  var url = create();
+  if (count > 10) {
+    chrome.tabs.create({url:url});
+    return;
+  }
+  getTitle(url, function(targetUrl, title) {
+    if (title == 'レシピ検索No.1／料理レシピ載せるなら クックパッド') {
+      run();
+    } else {
+      chrome.tabs.create({url:url});
+    }
+  });
+}
+
+function getTitle(url, callback) {
+  var xhr = new XMLHttpRequest()
+  xhr.onload = function() {
+    var title = this.responseXML.title || "no title"
+    callback(url, title)
+  }
+  xhr.open("GET", url, true);
+  xhr.responseType = "document";
+  xhr.send();
+}
+
+var count;
+
 chrome.browserAction.onClicked.addListener(function(activeTab) {
-  url='http://cookpad.com/recipe/' + Math.floor( Math.random() * 1840000 );
-  chrome.tabs.create( { url: url} );
+  count = 0;
+  run();
 });
