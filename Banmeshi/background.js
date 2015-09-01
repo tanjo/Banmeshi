05d9cdeae7cac45ad625155fc15882ad57abda4a
tanjo <tanjo@thebitcellar.com>
2015-09-01 19:11:27 +0900

    214万レシピ

--- Banmeshi/background.js
+++ Banmeshi/background.js
@@ -1,5 +1,5 @@
 function create() {
-  var url='http://cookpad.com/recipe/' + Math.floor( Math.random() * 2000000 );
+  var url='http://cookpad.com/recipe/' + Math.floor( Math.random() * 2140000 );
   return url;
 }
 
